314e07f287608047d417fb8871e21491631a5a6f
adriwitek <adrian.rubio.bsc@gmail.com>
2024-01-11 16:43:56 +0100

    assert commnads

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -7,15 +7,27 @@ import logging
 import shutil
 
 
-def process_doc(pdf_path, pipe,output_path):
+def process_doc(pdf_path, pipe, output_path, override = False):
     '''Processes and saves each pdf file'''
 
     # Create folder for output
     docname = os.path.basename(os.fsdecode(pdf_path)).split('.')[0]
     processed_output_folder= os.path.join(output_path,docname + '_processed')
+    finished_processed_output_folder= os.path.join(output_path,docname + '_processed_finished')
 
+
+    # Check doc is not already processed and if we want to override
+    if  os.path.exists(finished_processed_output_folder):
+        if(override):
+            #logging.warning('Directory: ' + str(processed_output_folder) + ' already exits. Overwriting it!')
+            shutil.rmtree(finished_processed_output_folder)           # Removes all the subdirectories!
+        else:
+            return 
+        
+
+    # Create temp folder
     if  os.path.exists(processed_output_folder):
-        logging.warning('Directory: ' + str(processed_output_folder) + ' already exits. Overwriting it!')
+        # If we are here, something went wrong so we overwrite files
         shutil.rmtree(processed_output_folder)           # Removes all the subdirectories!
     os.makedirs(processed_output_folder)
 
@@ -26,7 +38,7 @@ def process_doc(pdf_path, pipe,output_path):
 
         for page, (image, paragraphs) in enumerate(image_and_paragraphs):
             paragraphs_coords_and_labels = []
-            f.write('## PAGE:'+ str(page) + ' ##\n\n')
+            f.write('## PAGE:'+ str(page) + '##\n\n')
             for text, coords, img in paragraphs:
                 is_table = detect_table(img)
                 if not is_table: #Process if not table
@@ -45,6 +57,16 @@ def process_doc(pdf_path, pipe,output_path):
             get_results_visualization(image_output_path, image, paragraphs_coords_and_labels)
 
 
+    # Rename folder in order to reflext the process is finished
+    os.rename(processed_output_folder, finished_processed_output_folder)
+
+
+
+# Define a custom argument type for a list of strings
+def list_of_strings(arg):
+    return arg.split(',')
+
+
 
 
 def main(*args, **kwargs):
@@ -58,43 +80,91 @@ def main(*args, **kwargs):
 
     parser.add_argument("-i", 
                         "--input",
-                        help="PDF doc or directory with several pdf documents")
+                        default=None,
+                        help="PDF doc or directory with several pdf documents"
+                        )
+    
+
+    parser.add_argument('-l',
+                        '--list_of_inputs', 
+                        action='append', 
+                        default=None,
+                        help='List of docs to process. Each element must be preceded by the argument mark',
+                        )
+
     
     parser.add_argument("-o", 
                         "--output",
-                        help="Output path.")
+                        help="Output path."
+                        )
 
 
     parser.add_argument("-m", "--model", 
-                        default="pipeline/models/nextprocurement_pdfutils",
-                        help="Path to used doc-processing model.")
+                        default="/gpfs/projects/bsc88/projects/NextProcurement/model_trainer/output/NextProcurement/next_procurement_v0_8_0.00005_date_22-08-13_time_04-51-37",
+                        help="Path to used doc-processing model."
+                        )
+    
+    parser.add_argument('--override_output',
+                        action="store_true",
+                        help='if set overrides the output'
+                        )
+
+
+    if len(sys.argv)==1:
+        parser.print_help(sys.stderr)
+        sys.exit(1)
 
 
     args = parser.parse_args()
 
+
+    if(args.list_of_inputs is None and args.input is None):
+        err_msg = "ERROR: --input or  --list_of_inputs argument must be provided!"
+        raise  Exception( err_msg )
+
+
+    if(args.output is None):
+        err_msg = "ERROR: --output path must be provided!"
+        raise  Exception( err_msg )
+
      
-    # Load model
+    # Load model and pipeline
+    os.environ["TOKENIZERS_PARALLELISM"] = "false"
     tokenizer = AutoTokenizer.from_pretrained(str(args.model))
     model = AutoModelForSequenceClassification.from_pretrained(str(args.model))
+    #pipe = pipeline("text-classification", model=model, tokenizer=tokenizer)
+    pipe = pipeline("text-classification", model=model, tokenizer=tokenizer, device = 0)
 
-    # Load pipeline
-    pipe = pipeline("text-classification", model=model, tokenizer=tokenizer)
 
-
-    if(os.path.isdir(args.input) ):
+    # List of N docs to process (helpul when we want to paralellize)
+    if(args.list_of_inputs is not None):
+        for pdf_path in args.list_of_inputs:
+            filename = os.fsdecode(pdf_path)
+            if filename.endswith(".pdf"): 
+                try:
+                    process_doc(os.path.join(pdf_path), pipe, args.output, args.override_output)
+                except Exception as e:
+                    err_msg = "ERROR: An error ocurred parsing the document called: " + filename + '. \n\t' + str(e)
+                    #raise  Exception( err_msg)
+                    print(err_msg)
+            else:
+                logging.info('Skipping non-pdf file: ' + filename)
+    # An folder with docs
+    elif(os.path.isdir(args.input) ):
         for pdf_path in os.listdir(args.input):
             filename = os.fsdecode(pdf_path)
             if filename.endswith(".pdf"): 
                 try:
-                    process_doc(os.path.join(args.input,pdf_path), pipe, args.output)
+                    process_doc(os.path.join(args.input,pdf_path), pipe, args.output, args.override_output)
                 except Exception as e:
-                    err_msg = "An error ocurred parsing the document called: " + filename + '. \n\t' + str(e)
-                    raise  Exception( err_msg)
+                    err_msg = "ERROR: An error ocurred parsing the document called: " + filename + '. \n\t' + str(e)
+                    #raise  Exception( err_msg)
+                    print(err_msg)
             else:
                 logging.info('Skipping non-pdf file: ' + filename)
        
     else:#single input doc
-        process_doc(args.input, pipe, args.output)
+        process_doc(args.input, pipe, args.output, args.override_output)
 
 
 
